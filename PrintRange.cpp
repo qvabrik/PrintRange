32535a0c21c4d722b45fbff9e22934276efa7282
qvabrik <kirilljkjk@yandex.ru>
2022-05-18 15:22:14 +0300

    Добавьте файлы проекта.

---
+++ PrintRange.cpp
@@ -0,0 +1,13 @@
+﻿template<typename IteratorType>
+void PrintRange(IteratorType it, IteratorType end) {
+    bool isFirst = true;
+    for (; it != end; ++it) {
+        if (isFirst) {
+            cout << *it;
+            isFirst = false;
+            continue;
+        }
+        cout << " "s << *it;
+    }
+    cout << endl;
+}
